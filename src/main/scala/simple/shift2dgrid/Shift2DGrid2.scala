4bf452f7088d667704ab84bc11b1066576225163
Soumen <soumensaha@gmail.com>
2022-04-11 13:56:10 +0530

    2234. Shift 2D array

---
+++ src/main/scala/simple/shift2dgrid/Shift2DGrid2.scala
@@ -0,0 +1,19 @@
+package com.soumen.leetcodesolutions
+package simple.shift2dgrid
+
+/**
+ * https://leetcode.com/problems/shift-2d-grid/
+ */
+object Shift2DGrid2 extends App {
+  def shiftGrid(grid: Array[Array[Int]], k: Int): List[List[Int]] = (grid.head.length, grid.length) match {
+    case (m, n) => m * n match {
+      case total => k % total match {
+        case 0 => grid.map(_.toList).toList
+        case shift => (0 until total).toList.map(_ - shift).map(x => if (x < 0) total + x else x)
+          .map(x => grid(x / m)(x % m)).grouped(m).toList
+      }
+    }
+  }
+}
+//Runtime: 700 ms, faster than 100.00% of Scala online submissions for Shift 2D Grid.
+//Memory Usage: 57.4 MB, less than 100.00% of Scala online submissions for Shift 2D Grid.
