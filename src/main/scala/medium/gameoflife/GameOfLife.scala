2172a728d3ffc662eff2aa09fe95544d7b917e00
Soumen <soumensaha@gmail.com>
2022-04-12 16:25:06 +0530

    289. Game of Life

---
+++ src/main/scala/medium/gameoflife/GameOfLife.scala
@@ -0,0 +1,36 @@
+package com.soumen.leetcodesolutions
+package medium.gameoflife
+
+/**
+ * https://leetcode.com/problems/game-of-life/
+ * 289. Game of Life
+ */
+object GameOfLife extends App {
+  def gameOfLife(board: Array[Array[Int]]): Unit = (board.length, board(0).length) match {
+    case (m, n) =>
+      def neighbors(x: Int, y: Int) = (for {
+        i <- x - 1 to x + 1 if i >= 0 && i < m
+        j <- y - 1 to y + 1 if j >= 0 && j < n
+      } yield board(i)(j)).count(_ == 1) - board(x)(y)
+
+      (for {
+        i <- 0 until m
+        j <- 0 until n
+      } yield board(i)(j) match {
+        case 0 => if (neighbors(i, j) == 3) (i, j, 1) else (i, j, 0)
+        case 1 => neighbors(i, j) match {
+          case 2 | 3 => (i, j, 1)
+          case _ => (i, j, 0)
+        }
+      }).foreach { case (i, j, x) => board(i)(j) = x }
+  }
+
+  val board = Array(Array(0, 1, 0), Array(0, 0, 1), Array(1, 1, 1), Array(0, 0, 0))
+  gameOfLife(board)
+  for {
+    i <- board.indices
+    j <- board(0).indices
+  } yield println(s"$i, $j, ${board(i)(j)}")
+}
+//Runtime: 512 ms, faster than 100.00% of Scala online submissions for Game of Life.
+//Memory Usage: 53.8 MB, less than 100.00% of Scala online submissions for Game of Life.
