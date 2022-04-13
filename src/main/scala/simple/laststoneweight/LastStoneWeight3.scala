95e3d76de884fc97b7dfe08396a4804e3fd12605
Soumen <soumensaha@gmail.com>
2022-04-14 01:16:16 +0530

    1046. Last Stone Weight

---
+++ src/main/scala/simple/laststoneweight/LastStoneWeight3.scala
@@ -0,0 +1,14 @@
+package com.soumen.leetcodesolutions
+package simple.laststoneweight
+
+object LastStoneWeight3 extends App {
+  def lastStoneWeight(stones: Array[Int]): Int = {
+    val q = scala.collection.mutable.PriorityQueue.from(stones)
+    while (q.length > 1) q.enqueue(q.dequeue() - q.dequeue())
+    if (q.isEmpty) 0 else q.dequeue()
+  }
+
+  println(lastStoneWeight(Array(2, 7, 4, 1, 8, 1)))
+}
+//Runtime: 593 ms, faster than 86.67% of Scala online submissions for Last Stone Weight.
+//Memory Usage: 55.2 MB, less than 88.00% of Scala online submissions for Last Stone Weight.
