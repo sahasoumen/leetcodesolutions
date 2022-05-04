d9a4cad7a8a66eed16dce6fd86d7326b2bf554e9
Soumen <soumensaha@gmail.com>
2022-05-04 16:10:59 +0530

    1679. Max Number of K-Sum Pairs

---
+++ src/main/scala/medium/maxnumberofksumpairs/Solution.scala
@@ -0,0 +1,24 @@
+package com.soumen.leetcodesolutions
+package medium.maxnumberofksumpairs
+
+/**
+ * https://leetcode.com/problems/max-number-of-k-sum-pairs/
+ * 1679. Max Number of K-Sum Pairs
+ * */
+object Solution extends App {
+  @scala.annotation.tailrec
+  def maxOperations(nums: Array[Int], k: Int, pos: Int = 0, m: Map[Int, Int] = Map.empty, count: Int = 0): Int = if (pos >= nums.length) count else nums(pos) match {
+    case n => m get (k - n) match {
+      case None => m get n match {
+        case None => maxOperations(nums, k, pos + 1, m + (n -> 1), count)
+        case Some(o) => maxOperations(nums, k, pos + 1, m + (n -> (o + 1)), count)
+      }
+      case Some(1) => maxOperations(nums, k, pos + 1, m - (k - n), count + 1)
+      case Some(o) => maxOperations(nums, k, pos + 1, m + ((k - n) -> (o - 1)), count + 1)
+    }
+  }
+
+  println(maxOperations(Array(2, 5, 4, 4, 1, 3, 4, 4, 1, 4, 4, 1, 2, 1, 2, 2, 3, 2, 4, 2), 3))
+}
+//Runtime: 2217 ms, faster than 100.00% of Scala online submissions for Max Number of K-Sum Pairs.
+//Memory Usage: 136.2 MB, less than 100.00% of Scala online submissions for Max Number of K-Sum Pairs.
---
+++ src/test/scala/medium/maxnumberofksumpairs/SolutionTest.scala
@@ -0,0 +1,29 @@
+package com.soumen.leetcodesolutions
+package medium.maxnumberofksumpairs
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should
+
+class SolutionTest extends AnyFreeSpec with should.Matchers {
+  val solution = Solution
+  "Max Number of K-Sum Pairs" - {
+    "should work with simple example" in {
+      solution.maxOperations(Array(1, 2, 3, 4), 5) should be(2)
+    }
+    "should work with duplicate" in {
+      solution.maxOperations(Array(3, 1, 3, 4, 3), 6) should be(1)
+    }
+
+    "should be able to detect trivial arrays" in {
+      solution.maxOperations(Array(), 2) should be(0)
+      solution.maxOperations(Array(1), 2) should be(0)
+    }
+    "should be able to detect many duplicate elements" in {
+      solution.maxOperations(Array(2, 5, 4, 4, 1, 3, 4, 4, 1, 4, 4, 1, 2, 1, 2, 2, 3, 2, 4, 2), 3) should be(4)
+      solution.maxOperations(Array(2, 2, 2, 3, 1, 1, 4, 1), 4) should be(2)
+      solution.maxOperations(Array(2, 5, 4, 4, 1, 3, 4, 4, 1, 4, 4, 1, 2, 1, 2, 2, 3, 2, 4, 2), 3) should be(4)
+
+    }
+
+  }
+}
